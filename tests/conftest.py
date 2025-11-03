aef41c3b97e37ca858a518f1b9b2e18832995bd4
PavelD <447416+PavelD@users.noreply.github.com>
2025-11-03 01:34:41 +0100

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
 pytest_plugins = ["pytest_homeassistant_custom_component"]
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def enable_storyflow_integration(enable_custom_integrations):
+    """Ensure storyflow integration is enabled for all tests."""
+    yield
